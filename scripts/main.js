480d23fa2658dd3d0ca16deda22e6ff7c92dbc7b
GenYume <40338177+GenYume@users.noreply.github.com>
2018-06-17 16:47:38 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+/* eslint-env browser */
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/yinYang.svg') {
+        myImage.setAttribute ('src','images/osismiaci.svg');
+    } else {
+        myImage.setAttribute ('src','images/yinYang.svg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'This is ' + myName + '\'s website.';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'This is ' + storedName + '\'s website.';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
